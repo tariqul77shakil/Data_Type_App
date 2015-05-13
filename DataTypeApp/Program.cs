05c9bb557126982843a44d9da4dcb4f9d31350b4
tariqul77shakil <shakil77cse@gmail.com>
2015-05-13 15:51:15 +0600

    Data type app uploaded

---
+++ DataTypeApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int aNumber = 123;
+            //float bNumber = 12.4f;
+            //char a = 'r';
+            //string aText = "Seip Project.";
+            //bool check = false;
+            //Console.WriteLine(aNumber+bNumber);
+            //Console.ReadKey();
+
+            int sNumber;
+            int fNumber;
+            int choice;
+
+            Console.WriteLine("Enter first number");
+            fNumber = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter second number.");
+            sNumber = Convert.ToInt16(Console.ReadLine());
+
+
+        }
+    }
+}
